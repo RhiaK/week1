1858b34ccb5fe3716c97dcd771652400259095e7
RhiaKe <rhiakaulitzke@gmail.com>
2018-03-20 15:41:59 -0600

    adding homework

---
+++ w1d1_lab/first_lab.js
@@ -0,0 +1,65 @@
+/*console.log("Making your way in the world today takes everything you've got.");
+console.log("Taking a break from all your worries, sure would help a lot.");
+console.log("Wouldn't you like to get away?");
+console.log("Sometimes you want to go");
+console.log("Where everybody knows your name,");
+console.log("and they're always glad you came.");
+console.log("You wanna be where you can see,");
+console.log("our troubles are all the same");
+console.log("You wanna be where" + "everybody knows");
+console.log("Your name.");
+
+console.log(999 > 999);
+console.log(999 === 999);
+console.log(999 != 999);
+console.log(-5 >= -4);
+console.log(100 <=-100);
+console.log(20 + 5 < 5);
+console.log( 81 / 9 === 9);
+console.log(9 != 8 + 1);
+
+//The assignment operator assigns a variable to a name for that variable, an equality operator determines math equality.
+
+var i=1;
+while (i <= 1000) {
+	console.log('Ginger chocolate honey patties');
+	i++;
+}
+
+var num=0;
+while (num <= 1000) {
+	console.log(num);
+	num++;
+}
+
+var a=0;
+while (a <= 1000) {
+	console.log("The current loop number is " + a);
+}
+
+for (x = 0; x <= 100; x++) {
+	console.log(x);
+}
+
+for (r = 7; r <= 635; r++) {
+	console.log(r);
+} 
+
+let start = 0;
+const limit = 100;
+for (val = start; val <= limit; val++) {
+	console.log(val);
+} */
+
+//songs in a list
+
+let fin = last_song;
+for (song = 0; song <= fin; song++) {
+	play; //I would probably have to assign the function to the variable "play."
+}
+//I am guessing there should be an array listing out each song, but this for loop basically shows that each song will play in order.
+
+
+
+
+
