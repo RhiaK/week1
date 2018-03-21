a2a55171265c891454bdcef363072e8a6d052d6e
RhiaKe <rhiakaulitzke@gmail.com>
2018-03-20 21:04:48 -0600

    Created loops.js file

---
+++ loops.js
@@ -0,0 +1,41 @@
+let i=0;
+ while(i <= 99) {
+ 		console.log(i);
+ 		i++;
+ }
+
+let i=99;
+	while(i >= 0) {
+		console.log(i);
+		i--;
+ }
+
+let i=0;
+	while(i <= 98) {
+		console.log(i);
+		i = i + 2;
+ }
+
+let i=98;
+	while(i >= 0) {
+		console.log(i);
+		i = i - 2;
+ }
+ 
+let i=49;
+ 	while(i <= 72) {
+ 		console.log(i);
+ 		i++;
+ }
+
+let i=81;
+	while(i >=26) {
+		console.log(i);
+		i--;
+ }
+ 
+let i=3;
+ 	while(i <= 90) {
+ 		console.log(i);
+ 		i = i + 3;
+ } 
