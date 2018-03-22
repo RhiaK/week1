8140c957a51255370e740f71cf8c6a70f0fac757
RhiaKe <rhiakaulitzke@gmail.com>
2018-03-21 20:43:38 -0600

    Commit 3 - Excited Kittens Answered

--- wednesday/answers.js
+++ wednesday/answers.js
@@ -8,3 +8,12 @@ for (i = 0; i <= 200; i ++) {
 		console.log(i);
 	}
 }*/
+const cat = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+for (i = 1; i <= 20; i ++) {
+	if (i % 2 === 0) {
+		answer = cat[Math.floor(Math.random() * 3)];
+		console.log(answer);
+	} else {
+	console.log("Love me, pet me! HSSSSSS!");
+}
+}
