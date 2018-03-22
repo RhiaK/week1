7e79e725637b39354bbf00086274349878767419
RhiaKe <rhiakaulitzke@gmail.com>
2018-03-21 20:58:30 -0600

    Commit 4 - Fizz Buzz Answered

--- wednesday/answers.js
+++ wednesday/answers.js
@@ -7,7 +7,8 @@ for (i = 0; i <= 200; i ++) {
 	if (i % 2 === 0) {
 		console.log(i);
 	}
-}*/
+}
+//3.
 const cat = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
 for (i = 1; i <= 20; i ++) {
 	if (i % 2 === 0) {
@@ -16,4 +17,32 @@ for (i = 1; i <= 20; i ++) {
 	} else {
 	console.log("Love me, pet me! HSSSSSS!");
 }
+}*/
+//4.
+for (i = 1; i <= 100; i ++) {
+	if ((i % 3 === 0) && (i % 5 === 0)) {
+		console.log("FizzBuzz");
+	} else if (i % 3 === 0) {
+		console.log("Fizz");
+	} else if (i % 5 === 0) {
+		console.log("Buzz");
+	} else {
+		console.log(i);
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
